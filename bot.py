e261f8b26b45515c035cb953d19a7be45cce4291
westiemcb <50559087+westiemcb@users.noreply.github.com>
2019-07-03 08:54:07 +1200

    Setup data for POST request

--- bot.py
+++ bot.py
@@ -1,8 +1,34 @@
 import aiohttp
+import asyncio
 from bs4 import BeautifulSoup
 import json
 
-with open("creds.json", "r") as f:
-    data = json.loads(f.read())
+LOGIN_PAGE = 'https://student-sa-odd.victoria.ac.nz/Authentication/Login.aspx?ReturnUrl=%2fStudent.aspx'
+
+
+with open('creds.json', 'r') as f:
+    creds = json.loads(f.read())
     
-print(data)
+
+
+async def main():
+    data = {
+        '__VIEWSTATE':'',
+        '__VIEWSTATEGENERATOR':'',
+        '__SCROLLPOSITIONX':0,
+        '__SCROLLPOSITIONY':0,
+        '__EVENTTARGET':'',
+        '__EVENTARGUMENT':'',
+        '__EVENTVALIDATION':'',
+        'ctl00$MainContent$userName':creds['username'],
+        'ctl00$MainContent$password':creds['password'],
+        'ctl00$MainContent$doLogin':'Login',
+    }
+    print(data)
+    session = aiohttp.ClientSession()
+    async with await session.get(LOGIN_PAGE) as response:
+        print(await response.text())
+    await session.close()
+    
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
